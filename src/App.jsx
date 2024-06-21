dfc5a5ab4ae312cc9d332238417b6e8aeb4b04c3
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-06-21 09:59:33 +0530

    css reset and addition of header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 
+import Header from "./Components/Header/Header";
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <h1>Building the LinkedIn clone</h1>
     </>
   );
