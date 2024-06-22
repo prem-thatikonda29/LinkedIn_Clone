e7101269ed2ce058a56394a60d9bbaf1ba763d47
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-06-22 16:34:03 +0530

    finished homepage

--- src/Components/Widgets/Widgets.jsx
+++ src/Components/Widgets/Widgets.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./Widgets.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faInfoCircle, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 function Widgets() {
+  const newsArticle = (heading, subtitle) => {
+    return (
+      <div className={styles.widgets__article}>
+        <div className={styles.widget__article__left}>
+          <FontAwesomeIcon icon={faCircle} size="xs"/>
+        </div>
+        <div className={styles.widget__article__right}>
+          <h4>{heading}</h4>
+          <p>{subtitle}</p>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className={styles.widgets}>
       <div className={styles.widgets__header}>
         <h2>LinkedIn news</h2>
-        <FontAwesomeIcon icon={faInfoCircle} />
+        <FontAwesomeIcon icon={faInfoCircle} size="lg" />
       </div>
+      {newsArticle("React is killing me", "Top news from ITM")}
+      {newsArticle("Demo Days postponed!", "Students happy")}
+      {newsArticle("Chutti dedo :(", "The voice of all kids")}
     </div>
   );
 }
